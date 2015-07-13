3904cc456ebd2e61c2f168398ae12abf951e32cc
Barak Korren <bkorren@redhat.com>
2015-07-13 18:35:26 +0300

    WiP: Wrapper scrit can now setup Satellite

--- satellite-testing.bash
+++ satellite-testing.bash
@@ -6,8 +6,8 @@ main() {
     #env_json.generate
     #repo_json.generate
     #environment.init
-    environment.setup_hosts
-    #testenvcli.shell 'repo'
+    #environment.setup_hosts
+    hosts.setup_host satellite
 }
 
 get_env_configuration() {
@@ -30,34 +30,197 @@ environment.start() {
 }
 
 environment.setup_hosts() {
-    hosts.repo.setup
+    for host in repo satellite; do
+        hosts.setup_host "$host"
+    done
 }
 
-hosts.repo.setup() {
+hosts.setup_host() {
+    local host="${1:?}"
+
     hosts.run_remote_functions \
-        "repo" \
-        'hosts.repo.remote.setup' \
-        'hosts.remote.*' \
-        'hosts.repo.remote.*'
+        "$host" \
+        "hosts.${host}.remote.setup" \
+        "hosts.remote.*" \
+        "hosts.${host}.remote.*"
 }
 
 hosts.repo.remote.setup() {
-    local rh_mirror="http://download.eng.tlv.redhat.com/pub"
-    local rhel_mirror="${rh_mirror}/rhel/released/RHEL-7/7.1"
+    declare -g REPOMAN_CONF='/etc/repoman.conf'
+    declare -g REPO_BASE_DIR='/var/www/repos'
+    declare -g REPO_BASE_URL='repos'
+
+    local sat6_brew_tag='satellite-6.1.0-rhel-7-candidate'
+
     hosts.remote.yum.zaprepos
-    hosts.remote.yum.addrepo \
-        'rhel' \
-        "${rhel_mirror}/Server/x86_64/os"
-    hosts.remote.yum.addrepo \
-        'rhel-optional' \
-        "${rhel_mirror}/Server-optional/x86_64/os"
+    hosts.remote.yum.add_rhel_repos
     hosts.remote.yum.addrepo \
         'ci-tools' \
         'http://ci-web.eng.lab.tlv.redhat.com/repos/ci-tools/el7'
     hosts.remote.yum.addrepo \
         'rhpkg' \
         'http://download.lab.bos.redhat.com/rel-eng/dist-git/rhel/$releasever/'
-    yum install -y createrepo repoman httpd brewkoji
+    hosts.repo.remote.setup.repoman
+    hosts.repo.remote.setup.httpd
+    hosts.repo.remote.setup.brew_tag_repo \
+        'satellite' \
+        "$sat6_brew_tag" \
+        inherit
+}
+
+hosts.satellite.remote.setup() {
+    hosts.remote.yum.zaprepos
+    hosts.remote.yum.add_rhel_repos
+    hosts.remote.yum.addrepo \
+        'satellite' \
+        'http://repo/repos/satellite/$releasever'
+    hosts.remote.net.set_static
+    hosts.remote.net.set_self_resolv satellite.example.com satellite
+    hosts.remote.satellite.setup.satellite
+}
+
+hosts.repo.remote.setup.repoman() {
+    yum install -y createrepo repoman brewkoji
+    cat > "$REPOMAN_CONF" <<EOF
+[source.KojiBuildSource]
+koji_server = http://brewhub.devel.redhat.com/brewhub
+koji_topurl = http://download.devel.redhat.com/brewroot
+
+[store.RPMStore]
+# Some links that are expected by some rhel distro flavours
+extra_symlinks =
+	el6:6Server
+	el7:7Server
+	el7:7Everything
+# no need for src.rpms
+with_srcrpms = false
+# we don't need sources dir ds
+with_sources = false
+# don't create rpm subdir
+rpm_dir =
+EOF
+}
+
+hosts.repo.remote.setup.httpd() {
+    local HTTPD_CONF='/etc/httpd/conf.d/repos.conf'
+
+    yum install -y httpd
+    cat > "$HTTPD_CONF" <<EOF
+alias /$REPO_BASE_URL $REPO_BASE_DIR
+
+<Directory $REPO_BASE_DIR>
+    Options Indexes FollowSymLinks
+    AllowOverride None
+    Require all granted
+</Directory>
+EOF
+    install -o root -g root -m 755 -d "$REPO_BASE_DIR"
+    systemctl enable httpd
+    systemctl restart httpd
+    firewall-cmd --permanent --add-service=http
+    firewall-cmd --reload
+}
+
+hosts.repo.remote.setup.brew_tag_repo() {
+    local reponame="${1:?}"
+    local tag="${2:?}"
+    local inherit="${3}"
+    if [[ "${inherit,,}" == "inherit" ]]; then
+        inherit='@inherit'
+    else
+        inherit=''
+    fi
+    local repo_path="${REPO_BASE_DIR}/${reponame}"
+
+    install -o root -g root -m 755 -d "$repo_path"
+    repoman -c "$REPOMAN_CONF" "$repo_path" add "koji:@${tag}${inherit}"
+}
+
+hosts.remote.satellite.setup.satellite() {
+    local PRIVATE_IFACE='eth1'
+    local private_ip="$(hosts.remote.net.get_conn_ip "$PRIVATE_IFACE")"
+    local dhcp_range=(${private_ip%.*}.{10,100})
+    local dhcp_range="${dhcp_range[*]}"
+
+    yum install -y katello
+
+    local dns_zone="$(facter domain)"
+    local dns_zone="${dns_zone:-example.com}"
+
+    firewall-cmd --permanent --add-service=http
+    firewall-cmd --permanent --add-service=https
+    firewall-cmd --permanent --add-port=9090/tcp
+    firewall-cmd --reload
+
+    katello-installer \
+        --capsule-dns true \
+        --capsule-dns-forwarders "$private_ip" \
+        --capsule-dns-interface "$PRIVATE_IFACE" \
+        --capsule-dns-zone "$dns_zone"\
+        --capsule-dhcp true \
+        --capsule-dhcp-gateway "$private_ip" \
+        --capsule-dhcp-interface "$PRIVATE_IFACE" \
+        --capsule-dhcp-nameservers "$private_ip" \
+        --capsule-dhcp-range "$dhcp_range" \
+        --capsule-tftp true \
+        --capsule-tftp-servername "$private_ip"
+}
+
+hosts.remote.net.set_static() {
+    local PUBLIC_IFACE=eth0
+    local PRIVATE_IFACE=eth1
+    local PUBLIC_CONN="$PUBLIC_IFACE"
+    local PRIVATE_CONN="$PRIVATE_IFACE"
+    local PRIVATE_NET="192.168.105"
+
+    local public_ip="$(hosts.remote.net.get_conn_ip "$PUBLIC_IFACE")"
+    local private_ip="${public_ip/*./$PRIVATE_NET.}"
+
+    # If local connection already exists disable and delete it
+    if nmcli conn show "$PRIVATE_CONN" >> /dev/null 2>&1; then
+        nmcli conn down "$PRIVATE_CONN"
+        nmcli conn delete "$PRIVATE_CONN"
+    fi
+    # Drop all connections that use our interface
+    local ids_to_drop=($(
+        nmcli conn show \
+        | while read name uuid type device; do
+            [[ "$device" == "$PRIVATE_IFACE" ]] && echo "$uuid"
+        done
+    ))
+    for uuid in "${ids_to_drop[@]}"; do
+        echo "Setting connection $uuid down"
+        nmcli conn down "$uuid"
+    done
+    # Add a new connection
+    nmcli conn add \
+        type ethernet \
+        ifname "$PRIVATE_IFACE" \
+        con-name "$PRIVATE_CONN" \
+        ip4 "$private_ip/24"
+    nmcli conn up "$PRIVATE_CONN"
+}
+
+hosts.remote.net.set_self_resolv() {
+    local hostname="${1:?}"
+    shift
+
+    local PUBLIC_IFACE=eth0
+    local public_ip="$(hosts.remote.net.get_conn_ip "$PUBLIC_IFACE")"
+
+    local sedp="
+\$a \
+$public_ip $hostname "$@"
+/^${public_ip//\./\\.}/d
+"
+    sed -ire "$sedp" /etc/hosts
+}
+
+hosts.remote.net.get_conn_ip() {
+    local conn_name="${1:?}"
+
+    nmcli conn show "$conn_name" \
+    | sed -nre 's/IP4.ADDRESS\[[0-9]\]:\s+([0-9\.]+)\/[0-9]+/\1/p'
 }
 
 hosts.remote.yum.zaprepos() {
@@ -67,6 +230,21 @@ hosts.remote.yum.zaprepos() {
     | sort -u \
     | xargs -r yum remove -y
     rm -f /etc/yum.repos.d/*
+}
+
+hosts.remote.yum.add_rhel_repos() {
+    local rh_mirror="http://download.eng.tlv.redhat.com/pub"
+    local rhel_mirror="${rh_mirror}/rhel/released/RHEL-7/7.1"
+
+    hosts.remote.yum.addrepo \
+        'rhel' \
+        "${rhel_mirror}/Server/x86_64/os"
+    hosts.remote.yum.addrepo \
+        'rhel-optional' \
+        "${rhel_mirror}/Server-optional/x86_64/os"
+    hosts.remote.yum.addrepo \
+        'scl' \
+        "${rh_mirror}/rhel/released/RHSCL/2.0/RHEL-7/Server/x86_64/os"
 }
 
 hosts.remote.yum.addrepo() {
@@ -91,6 +269,8 @@ hosts.run_remote_functions() {
     #echo bundling functions: "${other_function_patterns[@]}"
 
     {
+        # Send TERM over so we get nicer output
+        echo "export TERM=$TERM"
         glob_functions "$main_function" "${other_function_patterns[@]}"
         echo "$main_function"
     } | testenvcli.shell "$host"
